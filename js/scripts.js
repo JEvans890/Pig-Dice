27f3b313780a58430f8047a3a27826a4de2cd0bf
JEvans890 <evansjexp@gmail.com>
2023-06-14 15:16:01 -0700

    complete UI displaying all business logic
    Co-authored-by: Thomas Bakken <tbakken14@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,40 @@ function isWinning(score) {
 }
 
 
-
 //UI Logic
+function handleEndTurn(game) {
+    game.endTurn();
+    updateDisplay(game);
+}
+
+function handleRoll(game) {
+    const rollValue = diceRoll();
+    game.rollLogic(rollValue);
+    updateDisplay(game);
+}
+
+function updateDisplay(game) {
+    let player1Score = document.querySelector(".score1");
+    let player2Score = document.querySelector(".score2");
+    let turn = document.querySelector("#turn h2");
+    let turnScore = document.querySelector("#turnScore #score");
+
+    player1Score.innerText = game.player1Score
+    player2Score.innerText = game.player2Score
+    if (game.isPlayer1Turn === true) {
+        turn.innerText = "Player1";
+    }
+    else {
+        turn.innerText = "Player2";
+    }
+    turn.innerText += ", it is your turn!";
+    turnScore.innerText = game.turnScore;
+}
+
+window.onload = function () {
+    let game = new GameState();
+    endTurnButton = document.getElementById("endTurn");
+    rollButton = document.getElementById("roll");
+    endTurnButton.addEventListener("click", function () { handleEndTurn(game) })
+    rollButton.addEventListener("click", () => handleRoll(game))
+}
